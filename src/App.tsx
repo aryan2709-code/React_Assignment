93fe9239ca703a3776fb876e485945e60d5e70f5
Aryan <bhardwaj7a@gmail.com>
2025-03-21 17:20:39 +0530

    Initialise a new react project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return <div className="bg-blue-300 text-white" >
+    Hello
+  </div>
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// Colors : 
+// Button color : #6c25ff
+// lower button color : #cebafb
+// welcome to popx text : #454a4e
+// Lower text : #f7f8f9
+// Color of the screen : #f7f8f9
+
