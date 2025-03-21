0d9662530272ca3dc076f97cd6aed74969eecfa8
Aryan <bhardwaj7a@gmail.com>
2025-03-21 17:36:20 +0530

    Define the routes and dummy pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-
-
+import { BrowserRouter , Routes , Route } from "react-router-dom"
+import { LandingPage } from "./pages/LandingPage"
+import { SignIn } from "./pages/SignInPage"
+import { SignUp } from "./pages/SignUpPage"
+import { Profile } from "./pages/ProfilePage"
 function App() {
-  return <div className="bg-blue-300 text-white" >
-    Hello
-  </div>
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/signin" element={<SignIn/>} />
+      <Route path="/signup" element={<SignUp/>} />
+      <Route path = "/profile" element = {<Profile/>} />
+    </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,7 @@
+export const LandingPage = () => {
+    return (
+        <div className="bg-black">
+            Landing Page
+        </div>
+    )
+}
